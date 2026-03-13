0f135db1de55c42928bc057f2efc98897d30c7c0
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2026-03-13 21:45:18 +0100

    Make footer link clickable

--- components/FooterBadge.vue
+++ components/FooterBadge.vue
@@ -1,9 +1,14 @@
 <template>
 	<hr class="w-full border-button-border my-0.5" />
-	<p class="m-0 flex items-center gap-1">
+	<a
+		href="https://github.com/creeperkatze/modrinth-extras"
+		target="_blank"
+		rel="noopener"
+		class="m-0 flex items-center gap-1 no-underline hover:underline"
+	>
 		<ExtLogo aria-hidden="true" class="h-4 w-4" />
 		Modrinth Extras {{ version }}
-	</p>
+	</a>
 </template>
 
 <script setup lang="ts">
