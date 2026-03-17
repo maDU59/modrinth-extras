c2bac59035c7196649a0eb7751a5206c68ece5da
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2026-03-17 21:03:28 +0100

    Rework linting & formatting, fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,65 +1,54 @@
 import eslint from '@eslint/js'
-import prettierConfig from 'eslint-config-prettier'
 import prettierPlugin from 'eslint-plugin-prettier/recommended'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import pluginVue from 'eslint-plugin-vue'
 import tseslint from 'typescript-eslint'
-
-const browserExtensionGlobals = {
-	// Standard browser globals
-	window: 'readonly',
-	document: 'readonly',
-	navigator: 'readonly',
-	console: 'readonly',
-	URL: 'readonly',
-	URLSearchParams: 'readonly',
-	fetch: 'readonly',
-	setTimeout: 'readonly',
-	clearTimeout: 'readonly',
-	setInterval: 'readonly',
-	clearInterval: 'readonly',
-	HTMLElement: 'readonly',
-	HTMLInputElement: 'readonly',
-	KeyboardEvent: 'readonly',
-	ResizeObserver: 'readonly',
-	MutationObserver: 'readonly',
-	// WebExtension globals
-	browser: 'readonly',
-	chrome: 'readonly',
-}
 
 export default tseslint.config(
 	eslint.configs.recommended,
 	...tseslint.configs.recommended,
 	...pluginVue.configs['flat/recommended'],
 	prettierPlugin,
-	prettierConfig,
 	{
+		languageOptions: {
+			globals: {
+				window: 'readonly',
+				document: 'readonly',
+				navigator: 'readonly',
+				console: 'readonly',
+				URL: 'readonly',
+				URLSearchParams: 'readonly',
+				fetch: 'readonly',
+				setTimeout: 'readonly',
+				clearTimeout: 'readonly',
+				setInterval: 'readonly',
+				clearInterval: 'readonly',
+				HTMLElement: 'readonly',
+				HTMLInputElement: 'readonly',
+				KeyboardEvent: 'readonly',
+				ResizeObserver: 'readonly',
+				MutationObserver: 'readonly',
+				browser: 'readonly',
+				chrome: 'readonly',
+			},
+		},
 		plugins: {
 			'simple-import-sort': simpleImportSort,
-		},
-		languageOptions: {
-			globals: browserExtensionGlobals,
 		},
 		rules: {
 			'simple-import-sort/imports': 'error',
 			'simple-import-sort/exports': 'error',
+			'vue/html-self-closing': 'off',
+			'vue/multi-word-component-names': 'off',
+			'@typescript-eslint/no-explicit-any': 'warn',
 		},
 	},
 	{
 		files: ['**/*.vue'],
 		languageOptions: {
-			globals: browserExtensionGlobals,
 			parserOptions: {
 				parser: tseslint.parser,
 			},
-		},
-	},
-	{
-		rules: {
-			'vue/html-self-closing': 'off',
-			'vue/multi-word-component-names': 'off',
-			'@typescript-eslint/no-explicit-any': 'warn',
 		},
 	},
 	{
